316846de8e21182c98907226656c07fa65e62a6b
khizriev <askhab.askhabb.khizriev@mail.ru>
2022-10-25 22:04:08 +0300

    izmenen

--- java.js
+++ java.js
@@ -1,5 +1,24 @@
+// первая задача вывести что-то в консол
 console.log("hello world");
-alert("открыт")
 
-let count = 50*55;
-console.log( count);
+// втарая задача вывести в alert я javaScript
+alert("я javaScript")
+
+// задача третье Работа с переменнымми
+ let admin, name;
+ name = "Djon";
+admin = name;
+alert( admin );
+
+// ЗАДАЧА-4 Переменная для названия нашей планеты
+let planetaName = Earth
+let   currentUserName = Djon
+
+
+                                               //ЗАДАЧА 5 
+
+/*Можно ли использовать заглавные буквы для имени birthday? А для age? Или одновременно для обеих переменных?
+
+Ответ: нет 
+обычно используется строчные буквы..
+
